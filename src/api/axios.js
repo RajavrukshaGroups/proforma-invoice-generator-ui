740bf1dd1879210c0eedf4dc99550a12b2e93e17
anupsr <sranup007@gmail.com>
2026-06-11 14:48:37 +0530

    new chnages

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:9500",
+  baseURL: "https://api.digitaleliteservices.in",
   // baseURL: "https://server.digitaleliteservices.in",
 });
 
